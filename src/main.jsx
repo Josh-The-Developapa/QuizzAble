322704bdd6fe62d4eedf8e87efb3aa49b353fff3
Emmanuel Asiimwe <asiimwemmanuel47@gmail.com>
2023-10-27 13:54:34 +0300

    spaces to tabs

--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	Link,
 } from "react-router-dom";
 import Home from './pages/Home/Home';
 import Login from './pages/Log/Login';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home/>,
-  },
-  {
-    path: "login",
-    element: <Login/>,
-  },
+	{
+		path: "/",
+		element: <Home/>,
+	},
+	{
+		path: "login",
+		element: <Login/>,
+	},
 ]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-     <RouterProvider router={router} />
-  </React.StrictMode>,
+	<React.StrictMode>
+		 <RouterProvider router={router} />
+	</React.StrictMode>,
 )
