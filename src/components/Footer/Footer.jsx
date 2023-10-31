b74aa4006d98f9c7f8fd488cabe992aa4cd84b73
Emmanuel Asiimwe <asiimwemmanuel47@gmail.com>
2023-10-31 11:47:24 +0300

    fixing footer props

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -45,108 +45,22 @@ function Footer() {
 					<ul style={{ listStyleType: "none", padding: 0 }}>
 						<li style={{ textAlign: "center" }}>
 							Joshua Mukisa Kiryowa, IB1 <br />{" "}
-							<b>Chairman, CTO</b>
+							<b>Founder & Chairman</b>
 						</li>
 						<li style={{ textAlign: "center" }}>
 							{/* <br /> */}
-							<br />
-							Emmanuel Asiimwe, IB1 <br />
-							<b>Development Manager</b>
+							<br /> Emmanuel Asiimwe, IB1 <br />
+							<b>Research & Development Manager</b>
 						</li>
 						<li style={{ textAlign: "center" }}>
 							{/* <br /> */}
-							<br />
-							Tahsan Samin, IB1 <br />
-							<b>Tech Lead</b>
+							<br /> Tahsan Samin, IB1 <br />
+							<b>Quality Assurance & Deployment Specialist</b>
 						</li>
-					</ul>
-				</div>
-			</div>
-			<div
-				style={{
-					margin: "20px",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					alignItems: "center",
-					flexWrap: "wrap",
-					minWidth: "400px",
-				}}
-			>
-				<h2 style={{ color: "black", marginBottom: "2px" }}>
-					Relevant members:
-				</h2>
-				<div
-					style={{
-						color: "#000000",
-						display: "flex",
-						justifyContent: "flex-start",
-						alignItems: "flex-start",
-					}}
-				>
-					<ul className="rm">
-						<div>
-							<li
-								style={{
-									textAlign: "center",
-									marginRight: "15px",
-								}}
-							>
-								<br />
-								Akhil Muni, IB1 <br />
-								<b>CFO</b> <b></b>
-							</li>
-							<li
-								style={{
-									textAlign: "center",
-									marginRight: "15px",
-								}}
-							>
-								<br /> Khush Shah, IB1 <br />
-								<b>CEO, Co-Founder</b>
-							</li>
-							<li
-								style={{
-									textAlign: "center",
-									marginRight: "15px",
-								}}
-							>
-								<br />
-								Joshua Obia, IB1 <br />
-								<b>COO</b> <b></b>
-							</li>
-						</div>
-						<div>
-							<li
-								style={{
-									textAlign: "center",
-									marginRight: "15px",
-								}}
-							>
-								<br />
-								Harsh Dhedhi, IB1 <br /> <b> CMO</b> <b></b>
-							</li>
-							<li
-								style={{
-									textAlign: "center",
-									marginRight: "15px",
-								}}
-							>
-								<br />
-								Alishaan, IB1 <br /> <b>Assistant CMO</b>{" "}
-								<b></b>
-							</li>
-							<li
-								style={{
-									textAlign: "center",
-									marginRight: "15px",
-								}}
-							>
-								<br />
-								Netsanet Mengistie, IB1 <br />{" "}
-								<b>Head of Security</b>
-							</li>
-						</div>
+						<li style={{ textAlign: "center" }}>
+							<br /> Khush Shah, IB1 <br />
+							<b>Marketing and Public Relations Strategist</b>
+						</li>
 					</ul>
 				</div>
 			</div>
